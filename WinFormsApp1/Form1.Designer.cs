2fe9fd3894f3b86aec6eca0b3b0b2b5700ddab52
StefanYves <stefannisipeanu02@gmail.com>
2023-05-08 00:54:00 +0300

    first commid

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Cerc = new System.Windows.Forms.Button();
+            this.Dreptunghi = new System.Windows.Forms.Button();
+            this.Patrat = new System.Windows.Forms.Button();
+            this.razaTextBox = new System.Windows.Forms.TextBox();
+            this.lungimeTextBox = new System.Windows.Forms.TextBox();
+            this.latimeTextBox = new System.Windows.Forms.TextBox();
+            this.laturaTextBox = new System.Windows.Forms.TextBox();
+            this.Triunghi = new System.Windows.Forms.Button();
+            this.latura3TextBox = new System.Windows.Forms.TextBox();
+            this.latura2TextBox = new System.Windows.Forms.TextBox();
+            this.latura1TextBox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ResetarePanel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Cerc
+            // 
+            this.Cerc.Location = new System.Drawing.Point(31, 487);
+            this.Cerc.Name = "Cerc";
+            this.Cerc.Size = new System.Drawing.Size(112, 34);
+            this.Cerc.TabIndex = 0;
+            this.Cerc.Text = "Cerc";
+            this.Cerc.UseVisualStyleBackColor = true;
+            this.Cerc.Click += new System.EventHandler(this.Cerc_Click);
+            // 
+            // Dreptunghi
+            // 
+            this.Dreptunghi.Location = new System.Drawing.Point(222, 487);
+            this.Dreptunghi.Name = "Dreptunghi";
+            this.Dreptunghi.Size = new System.Drawing.Size(112, 34);
+            this.Dreptunghi.TabIndex = 2;
+            this.Dreptunghi.Text = "Dreptunghi";
+            this.Dreptunghi.UseVisualStyleBackColor = true;
+            this.Dreptunghi.Click += new System.EventHandler(this.Dreptunghi_Click);
+            // 
+            // Patrat
+            // 
+            this.Patrat.Location = new System.Drawing.Point(582, 485);
+            this.Patrat.Name = "Patrat";
+            this.Patrat.Size = new System.Drawing.Size(112, 34);
+            this.Patrat.TabIndex = 3;
+            this.Patrat.Text = "Patrat";
+            this.Patrat.UseVisualStyleBackColor = true;
+            this.Patrat.Click += new System.EventHandler(this.Patrat_Click);
+            // 
+            // razaTextBox
+            // 
+            this.razaTextBox.Location = new System.Drawing.Point(31, 435);
+            this.razaTextBox.Name = "razaTextBox";
+            this.razaTextBox.PlaceholderText = "Raza";
+            this.razaTextBox.Size = new System.Drawing.Size(112, 31);
+            this.razaTextBox.TabIndex = 6;
+            // 
+            // lungimeTextBox
+            // 
+            this.lungimeTextBox.Location = new System.Drawing.Point(222, 435);
+            this.lungimeTextBox.Name = "lungimeTextBox";
+            this.lungimeTextBox.PlaceholderText = "Lungime";
+            this.lungimeTextBox.Size = new System.Drawing.Size(112, 31);
+            this.lungimeTextBox.TabIndex = 7;
+            // 
+            // latimeTextBox
+            // 
+            this.latimeTextBox.Location = new System.Drawing.Point(222, 386);
+            this.latimeTextBox.Name = "latimeTextBox";
+            this.latimeTextBox.PlaceholderText = "Latime";
+            this.latimeTextBox.Size = new System.Drawing.Size(112, 31);
+            this.latimeTextBox.TabIndex = 8;
+            // 
+            // laturaTextBox
+            // 
+            this.laturaTextBox.Location = new System.Drawing.Point(582, 435);
+            this.laturaTextBox.Name = "laturaTextBox";
+            this.laturaTextBox.PlaceholderText = "Latura";
+            this.laturaTextBox.Size = new System.Drawing.Size(112, 31);
+            this.laturaTextBox.TabIndex = 9;
+            // 
+            // Triunghi
+            // 
+            this.Triunghi.Location = new System.Drawing.Point(393, 488);
+            this.Triunghi.Name = "Triunghi";
+            this.Triunghi.Size = new System.Drawing.Size(112, 34);
+            this.Triunghi.TabIndex = 10;
+            this.Triunghi.Text = "Triunghi";
+            this.Triunghi.UseVisualStyleBackColor = true;
+            this.Triunghi.Click += new System.EventHandler(this.Triunghi_Click);
+            // 
+            // latura3TextBox
+            // 
+            this.latura3TextBox.Location = new System.Drawing.Point(393, 438);
+            this.latura3TextBox.Name = "latura3TextBox";
+            this.latura3TextBox.PlaceholderText = "Latura3";
+            this.latura3TextBox.Size = new System.Drawing.Size(112, 31);
+            this.latura3TextBox.TabIndex = 11;
+            // 
+            // latura2TextBox
+            // 
+            this.latura2TextBox.Location = new System.Drawing.Point(393, 389);
+            this.latura2TextBox.Name = "latura2TextBox";
+            this.latura2TextBox.PlaceholderText = "Latura2";
+            this.latura2TextBox.Size = new System.Drawing.Size(112, 31);
+            this.latura2TextBox.TabIndex = 12;
+            // 
+            // latura1TextBox
+            // 
+            this.latura1TextBox.Location = new System.Drawing.Point(393, 342);
+            this.latura1TextBox.Name = "latura1TextBox";
+            this.latura1TextBox.PlaceholderText = "Latura1";
+            this.latura1TextBox.Size = new System.Drawing.Size(112, 31);
+            this.latura1TextBox.TabIndex = 13;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(841, 269);
+            this.panel1.TabIndex = 14;
+            // 
+            // ResetarePanel
+            // 
+            this.ResetarePanel.Location = new System.Drawing.Point(716, 485);
+            this.ResetarePanel.Name = "ResetarePanel";
+            this.ResetarePanel.Size = new System.Drawing.Size(137, 34);
+            this.ResetarePanel.TabIndex = 15;
+            this.ResetarePanel.Text = "Resetare Panel";
+            this.ResetarePanel.UseVisualStyleBackColor = true;
+            this.ResetarePanel.Click += new System.EventHandler(this.ResetarePanel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 541);
+            this.Controls.Add(this.ResetarePanel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.latura1TextBox);
+            this.Controls.Add(this.latura2TextBox);
+            this.Controls.Add(this.latura3TextBox);
+            this.Controls.Add(this.Triunghi);
+            this.Controls.Add(this.laturaTextBox);
+            this.Controls.Add(this.latimeTextBox);
+            this.Controls.Add(this.lungimeTextBox);
+            this.Controls.Add(this.razaTextBox);
+            this.Controls.Add(this.Patrat);
+            this.Controls.Add(this.Dreptunghi);
+            this.Controls.Add(this.Cerc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button Cerc;
+        private Button Dreptunghi;
+        private Button Patrat;
+        private TextBox razaTextBox;
+        private TextBox lungimeTextBox;
+        private TextBox latimeTextBox;
+        private TextBox laturaTextBox;
+        private Button Triunghi;
+        private TextBox latura3TextBox;
+        private TextBox latura2TextBox;
+        private TextBox latura1TextBox;
+        private Panel panel1;
+        private Button ResetarePanel;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,216 @@
+﻿using ShapeDLL;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Security.Policy;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string laturi = "laturi.txt";
+        string arii = "arii.txt";
+        private void Cerc_Click(object sender, EventArgs e)
+        {
+            double radius = double.Parse(razaTextBox.Text);
+            
+            Circle circle = new Circle(radius);
+            double area = circle.CalculateArea();
+            MessageBox.Show($"Aria cercului este: {area}");
+
+            using (StreamWriter writer = new StreamWriter(laturi, true)) 
+            {
+                writer.WriteLine($"Raza cercului este: {radius}");
+            }
+
+            using (StreamWriter writer = new StreamWriter(arii, true)) 
+            {
+                writer.WriteLine($"Aria cercului este: {area}");
+            }
+
+
+            // Desenarea cercului în panoul de desenare
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Blue, 2);
+            g.DrawEllipse(p, 10, 10, (float)radius * 2, (float)radius * 2);
+
+        }
+
+        
+
+        private void Dreptunghi_Click(object sender, EventArgs e)
+        {
+            double width = double.Parse(latimeTextBox.Text);
+            double height = double.Parse(lungimeTextBox.Text);
+            ShapeDLL.Rectangle rectangle = new ShapeDLL.Rectangle(width, height);
+            double area = rectangle.CalculateArea();
+            MessageBox.Show($"Aria dreptunghiului este: {area}");
+
+            using (StreamWriter writer = new StreamWriter(laturi, true))
+            {
+                writer.WriteLine($"Laturile dreptunghiului sunt: {width}, {height}");
+            }
+
+            using (StreamWriter writer = new StreamWriter(arii, true))
+            {
+                writer.WriteLine($"Aria dreptunghiului este: {area}");
+            }
+
+            // Desenarea dreptunghiului în panoul de desenare
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Red, 2);
+            g.DrawRectangle(p, 10, 10, (float)width, (float)height);
+
+        }
+
+      
+        private void Patrat_Click(object sender, EventArgs e)
+        {
+            double side = double.Parse(laturaTextBox.Text);
+            Square square = new Square(side);
+            double area = square.CalculateArea();
+            MessageBox.Show($"Aria patratului este: {area}");
+
+            using (StreamWriter writer = new StreamWriter(laturi, true))
+            {
+                writer.WriteLine($"Latura patratului este: {side}");
+            }
+
+            using (StreamWriter writer = new StreamWriter(arii, true))
+            {
+                writer.WriteLine($"Aria patratului este: {area}");
+            }
+
+            //  Desenarea patratului în panoul de desenare
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Red, 2);
+            g.DrawRectangle(p, 10, 10, (float)side, (float)side);
+
+        }
+
+        private void Triunghi_Click(object sender, EventArgs e)
+        {
+           
+            double sideA = double.Parse(latura1TextBox.Text);
+            double sideB = double.Parse(latura2TextBox.Text);
+            double sideC = double.Parse(latura3TextBox.Text);
+
+           
+            if (sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA)
+            {
+                // Determinam tipu triunghiului
+                if (sideA == sideB && sideB == sideC)
+                {
+                    // Triunghiul este echilateral
+                    double area = Math.Sqrt(3) / 4 * sideA * sideA;
+                    MessageBox.Show($"Aria triunghiului echilateral este: {area}");
+
+                    using (StreamWriter writer = new StreamWriter(laturi, true))
+                    {
+                        writer.WriteLine($"Laturie triunghiului echilateral este: {sideA}, {sideB}, {sideC}");
+                    }
+
+                    using (StreamWriter writer = new StreamWriter(arii, true))
+                    {
+                        writer.WriteLine($"Aria triunghiului echilateral este: {area}");
+                    }
+
+                    // Adaugă cod pentru desenarea triunghiului în panoul de desenare
+                    Graphics g = panel1.CreateGraphics();
+                    Pen p = new Pen(Color.Blue, 2);
+
+                    // Calculează coordonatele punctelor triunghiului
+                    PointF pointA = new PointF(10, 10);
+                    PointF pointB = new PointF(10 + (float)sideB, 10);
+                    float angleC = (float)Math.Acos((sideA * sideA + sideB * sideB - sideC * sideC) / (2 * sideA * sideB));
+                    PointF pointC = new PointF(10 + (float)(sideA * Math.Cos(angleC)), 10 + (float)(sideA * Math.Sin(angleC)));
+
+                    // Desenează triunghiul
+                    g.DrawLine(p, pointA, pointB);
+                    g.DrawLine(p, pointB, pointC);
+                    g.DrawLine(p, pointC, pointA);
+                }
+                else if (sideA == sideB || sideA == sideC || sideB == sideC)
+                {
+                    // Triunghiul este isoscel
+                    double baseSide = sideA == sideB ? sideC : (sideA == sideC ? sideB : sideA);
+                    double height = Math.Sqrt(sideA * sideA - baseSide * baseSide / 4);
+                    double area = baseSide * height / 2;
+                    MessageBox.Show($"Aria triunghiului isoscel este: {area}");
+
+                    using (StreamWriter writer = new StreamWriter(laturi, true))
+                    {
+                        writer.WriteLine($"Laturie triunghiului isoscel este: {sideA}, {sideB}, {sideC}");
+                    }
+
+                    using (StreamWriter writer = new StreamWriter(arii, true))
+                    {
+                        writer.WriteLine($"Aria triunghiului isoscel este: {area}");
+                    }
+
+                    // Adaugă cod pentru desenarea triunghiului în panoul de desenare
+                    Graphics g = panel1.CreateGraphics();
+                    Pen p = new Pen(Color.Blue, 2);
+
+                    // Calculează coordonatele punctelor triunghiului
+                    PointF pointA = new PointF(10, 10);
+                    PointF pointB = new PointF(10 + (float)sideB, 10);
+                    float angleC = (float)Math.Acos((sideA * sideA + sideB * sideB - sideC * sideC) / (2 * sideA * sideB));
+                    PointF pointC = new PointF(10 + (float)(sideA * Math.Cos(angleC)), 10 + (float)(sideA * Math.Sin(angleC)));
+
+                    // Desenează triunghiul
+                    g.DrawLine(p, pointA, pointB);
+                    g.DrawLine(p, pointB, pointC);
+                    g.DrawLine(p, pointC, pointA);
+                }
+                else
+                {
+                    // Triunghiul este oarecare
+                    double s = (sideA + sideB + sideC) / 2;
+                    double area = Math.Sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+                    MessageBox.Show($"Aria triunghiului oarecare este: {area}");
+
+                    using (StreamWriter writer = new StreamWriter(laturi, true))
+                    {
+                        writer.WriteLine($"Laturie triunghiului oarecare este: {sideA}, {sideB}, {sideC}");
+                    }
+
+                    using (StreamWriter writer = new StreamWriter(arii, true))
+                    {
+                        writer.WriteLine($"Aria triunghiului oarecare este: {area}");
+                    }
+
+                    // Adaugă cod pentru desenarea triunghiului în panoul de desenare
+                    Graphics g = panel1.CreateGraphics();
+                    Pen p = new Pen(Color.Blue, 2);
+
+                    // Calculează coordonatele punctelor triunghiului
+                    PointF pointA = new PointF(10, 10);
+                    PointF pointB = new PointF(10 + (float)sideB, 10);
+                    float angleC = (float)Math.Acos((sideA * sideA + sideB * sideB - sideC * sideC) / (2 * sideA * sideB));
+                    PointF pointC = new PointF(10 + (float)(sideA * Math.Cos(angleC)), 10 + (float)(sideA * Math.Sin(angleC)));
+
+                    // Desenează triunghiul
+                    g.DrawLine(p, pointA, pointB);
+                    g.DrawLine(p, pointB, pointC);
+                    g.DrawLine(p, pointC, pointA);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Input invalid.");
+            }
+        }
+
+        private void ResetarePanel_Click(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+        }
+    }
+}
